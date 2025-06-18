5298888bd0f14513b95bc583391ceee599b5bba8
Stephan Higuti <stephan.higuti@sinch.com>
2025-06-18 13:40:07 +0200

    fix(smpp): we should not replace messageid with empty string

--- smpp.go
+++ smpp.go
@@ -35,15 +35,9 @@ func (c *K6SMPPClient) Bind(transmitterAddrs []string, receiverAddrs []string, c
 // Modify the SubmitMT wrapper function
 func (c *K6SMPPClient) SubmitMT(destinationMSISDN string, text string, sourceAddr string, tlvs map[pdutlv.Tag]interface{}, registeredDelivery uint8) MTSubmitResponse {
 	messageID, err := c.SMPPClient.SubmitMT(destinationMSISDN, text, sourceAddr, tlvs, registeredDelivery)
-	if err != nil {
-		return MTSubmitResponse{
-			MessageID: "",
-			Error:     err,
-		}
-	}
 	return MTSubmitResponse{
 		MessageID: messageID,
-		Error:     nil,
+		Error:     err,
 	}
 }
 
